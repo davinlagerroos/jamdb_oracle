a2c8c1125bf109d6c2ffac5fd688634a4077464f
Davin Lagerroos <dlagerro@umn.edu>
2017-01-02 15:20:51 -0600

    WIP Add support for different tns strings

--- src/jamdb_oracle_tns_encoder.erl
+++ src/jamdb_oracle_tns_encoder.erl
@@ -44,20 +44,10 @@ encode_record(auth, EnvOpts, Sess, Salt) ->
     >>,
     KeyConn}.
 
-encode_record(login, EnvOpts) ->
-    {ok, UserHost}  = inet:gethostname(),
-    User            = proplists:get_value(user, EnvOpts),
-    Host            = proplists:get_value(host, EnvOpts, ?DEF_HOST), 
-    Port            = proplists:get_value(port, EnvOpts, ?DEF_PORT),
-    Sid             = proplists:get_value(sid, EnvOpts, ?DEF_SID),
-    AppName         = proplists:get_value(app_name, EnvOpts, "jamdb"),
-    Data = unicode:characters_to_binary(
-    "(DESCRIPTION=(CONNECT_DATA=(SID="++Sid++
-    ")(CID=(PROGRAM="++AppName++
-    ")(HOST="++UserHost++")(USER="++User++
-    ")))(ADDRESS=(PROTOCOL=TCP)(HOST="++Host++
-    ")(PORT="++integer_to_list(Port)++")))"),
-    <<   
+encode_record(login, EnvOpts) when is_list(EnvOpts)->
+  encode_record(login, tns_string(EnvOpts));
+encode_record(login, Data) when is_binary(Data) ->
+    <<
     1,57,		  % Packet version number
     1,57,		  % Lowest compatible version number
     0,0,		  % Global service options supported
@@ -210,6 +200,36 @@ encode_record(close, Cursors) ->
     ?TTI_FUN,
     (encode_ub2(?TTI_LOGOFF))/binary
     >>.
+
+tns_string([{service_name, _}] = EnvOpts) ->
+  {ok, UserHost}  = inet:gethostname(),
+  User            = proplists:get_value(user, EnvOpts),
+  Host            = proplists:get_value(host, EnvOpts, ?DEF_HOST),
+  Port            = proplists:get_value(port, EnvOpts, ?DEF_PORT),
+  ServiceName     = proplists:get_value(service_name, EnvOpts),
+  AppName         = proplists:get_value(app_name, EnvOpts),
+  unicode:characters_to_binary(
+  "(DESCRIPTION=(CONNECT_DATA=(SERVICE_NAME="++ServiceName++
+  "))(ADDRESS=(PROTOCOL=TCP)(HOST="++Host++
+  ")(PORT="++integer_to_list(Port)++")))"),
+  ")(CID=(PROGRAM="++AppName++
+  ")(HOST="++UserHost++")(USER="++User++
+  ")))(ADDRESS=(PROTOCOL=TCP)(HOST="++Host++
+  ")(PORT="++integer_to_list(Port)++")))");
+tns_string([{sid, _}] = EnvOpts) ->
+  {ok, UserHost}  = inet:gethostname(),
+  User            = proplists:get_value(user, EnvOpts),
+  Host            = proplists:get_value(host, EnvOpts, ?DEF_HOST),
+  Port            = proplists:get_value(port, EnvOpts, ?DEF_PORT),
+  Sid             = proplists:get_value(sid, EnvOpts, ?DEF_SID),
+  AppName         = proplists:get_value(app_name, EnvOpts, "jamdb"),
+  Data = unicode:characters_to_binary(
+  "(DESCRIPTION=(CONNECT_DATA=(SID="++Sid++
+  ")(CID=(PROGRAM="++AppName++
+  ")(HOST="++UserHost++")(USER="++User++
+  ")))(ADDRESS=(PROTOCOL=TCP)(HOST="++Host++
+  ")(PORT="++integer_to_list(Port)++")))")
+  .
 
 setopts(all8, N) ->
     lists:nth(N,[[1,0,0,0,0,0,0,1,0,0,0,0,0],[1,1,0,0,0,0,0,0,0,0,0,0,0],[0,10,0,0,0,0,0,1,0,0,0,0,0]]).
